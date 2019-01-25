d99a6011a6415162df593c0d47dcdb37510611ed
Omer Arie Goldberg <omergoldberg@fb.com>
2019-01-25 15:34:55 +0200

    Moved python files to python dir

--- python/script.py
+++ python/script.py
@@ -24,11 +24,10 @@ class Status(Enum):
 def main():
     pp = pprint.PrettyPrinter(indent=4)
     state = json.loads(sys.argv[1])
-    pp.pprint(state)
-    # This crashes here -> check how you are accessing the object with enums
-    status = state[State.QuestionInstance.name][QInst.status.name]
-    questionInstance = state[State.QuestionInstance.name]
-    question = state[State.Question.name]
+    pp.pprint(state) 
+    status = state[State.questionInstance.name][QInst.status.name]
+    questionInstance = state[State.questionInstance.name]
+    question = state[State.question.name]
     if status == Status.correct.name or status == Status.incorrectNoAttempts.name:
         state[State.question.name] = helper.getNextQuestion(question['Assignment'])
         state[State.questionInstance.name] = {
@@ -54,4 +53,5 @@ def main():
 if __name__ == "__main__":
     main()
 
-# example of json parameter that script recieves and returns
+#example of json parameter that script recieves and returns
+#'{"question":{"Assignment": "ClimateChange","level": 1,"number": 2,"text": "Which variables are significant in the model? We will consider a variable signficant only if the p-value is below 0.05. (Select all that apply.)","options": ["MEI", "CO2", "CH4", "N20", "CFC.11", "CFC.12", "TSI", "Aerosols"],"correctAnswer": ["MEI", "CO2", "CFC.11", "CFC.12", "TSI", "Aerosols"],"explanation": "If you look at the model we created in the previous problem using summary(climatelm), all of the variables have at least one star except for CH4 and N2O. So MEI, CO2, CFC.11, CFC.12, TSI, and Aerosols are all significant.","attemptsOverall": 2, "weight": 0},"questionInstance":{"status": "newQuestion","answer": ["MEI", "CO2"],"numAttempts": 0},"user":{"username": "omer"}}'
