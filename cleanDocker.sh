6802c1751af65d5e3f98615aeba99ab194bbc869
Galit <glukin@mit.edu>
2019-02-04 23:48:55 -0500

    fixing cleanDocker

--- cleanDocker.sh
+++ cleanDocker.sh
@@ -1,11 +1,12 @@
+c="7e70d62cd078"
 if ! test -z "$(docker ps -q)"; then
   docker kill $(docker ps -q)
 fi
 docker rm $(docker ps -a -q)
-for id in $(docker images -q)
+for id in $(docker images -q);
 do
-  if "$id" -ne "7e70d62cd078"; then
-    docker rmi $id
+  if [ "$id" != "$c" ]; then
+    docker rmi $id -f;
   fi
 done
 docker build .
