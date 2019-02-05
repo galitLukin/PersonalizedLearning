224a815da52c813b6ed182b4da3ed54e4402fd43
Galit <glukin@mit.edu>
2019-02-04 20:59:45 -0500

    returning correct question based on user location

--- python/helper.py
+++ python/helper.py
@@ -1,7 +1,6 @@
 import json
 import networkx as nx
 import re
-import random
 
 def isCorrect(answer, correctAnswer):
 	if len(answer) is not len(correctAnswer):
@@ -43,37 +42,33 @@ def getNextQuestion(assignment, level, number):
 	with open('./python/LinearRegression.json', encoding='utf-8') as f:
 	    questions = json.load(f)
 
-	# j = julia.Julia()
-	# pred = j.include("./python/prediction.jl")
-	# treatment = j.func(assignment, level, history)
-
 	#should recieve this from omer
 	history = {"Gender" : "None", "level_of_education" : "None", "enrollment_mode" : "audit",\
 	"ageCategory" : "Null", "ad1" : 1, "ad2" : 1, "ad3" : 1, "ad4" : 1,\
 	"sd1" : 1, "sd2" : 1, "sd3" : 1, "sd4" : 1, \
 	"de1" : 1, "de2" : 1, "de3" : 0.8888888888888888, "de4" : 1,\
-	"score1" : 1, "score2" : 0, "score3" : 0, "score4" : 0}
+	"score1" : 1, "score2" : 0, "score3" : 0, "score4" : 0,\
+	"next1": 2, "next2": 1, "next3": 1, "next4": 1}
 
 	lnr = nx.nx_pydot.read_dot('./python/model/{}/{}/pytree.dot'.format(level,asmt))
 	treatment = '1'
+
 	#TODO deal with possible infinite loop
 	while treatment not in ["A","B","C"]:
 		treatment = getNextNode(history,lnr,treatment)
+
 	assignment = assignment.replace(" ", "")
 	if treatment == "A":
-        #TODO: return correct json based on where the user is
-		q = random.randint(0,4)
+		q = history["next{}".format(level - 1)] - 1
 		return questions[assignment][level - 2]['questions'][q]
 	elif treatment == "B":
-		#TODO:return correct json based on where the user is
-		q = random.randint(0,4)
+		q = history["next{}".format(level)] - 1
 		return questions[assignment][level - 1]['questions'][q]
 	else:
-		#TODO:return correct json based on where the user is
-		#TODO: deal with finishing the assignment
 		if level == 4:
-			q = random.randint(0,4)
-			return questions[assignment][level - 1]['questions'][q]
+			#TODO: deal with finishing the assignment
+			q = history["next{}".format(level)]
+			return questions[assignment][level - 1]['questions'][q] - 1
 		else:
-			q = random.randint(0,4)
+			q = history["next{}".format(level + 1)] - 1
 			return questions[assignment][level]['questions'][q]
