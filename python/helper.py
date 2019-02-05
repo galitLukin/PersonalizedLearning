740176c9e5ff48cbdf46fb9a5e84fc6874d3f763
Galit <glukin@mit.edu>
2019-02-04 20:39:56 -0500

    working predict func in python

--- python/helper.py
+++ python/helper.py
@@ -1,6 +1,7 @@
 import json
+import networkx as nx
+import re
 import random
-import julia
 
 def isCorrect(answer, correctAnswer):
 	if len(answer) is not len(correctAnswer):
@@ -10,16 +11,54 @@ def isCorrect(answer, correctAnswer):
 	else:
 		return True
 
+def getNextNode(history,lnr,curr):
+	root_exp = nx.get_node_attributes(lnr,'label')[curr]
+	root_exp = re.search('"(.*)"',root_exp).group(1)
+	#TODO: deal with if feature not in feature space - return rand or smthing smart
+	text = root_exp.split()
+	feature = text[0]
+	if feature == "Prescribe":
+		return text[1]
+	else:
+		val = history[feature]
+		comp = text[1]
+		threshold = text[2]
+		direction = "right"
+		if comp == "in":
+			if val in threshold:
+				direction = "left"
+		else:
+			if val < float(threshold):
+				direction = "left"
+		if direction == "left":
+			children = list(map(int, list(lnr.successors(curr))))
+			return str(min(children))
+		else:
+			children = list(map(int, list(lnr.successors(curr))))
+			return str(max(children))
+
 def getNextQuestion(assignment, level, number):
+	map={"Climate Change": "cc", "Reading Test Scores": "rts", "Detecting Flu Epedemics": "dfe"}
+	asmt = map[assignment];
 	with open('./python/LinearRegression.json', encoding='utf-8') as f:
 	    questions = json.load(f)
 
-	#should recieve this from omer
-	history = None
+	# j = julia.Julia()
+	# pred = j.include("./python/prediction.jl")
+	# treatment = j.func(assignment, level, history)
 
-	j = julia.Julia()
-	pred = j.include("./python/prediction.jl")
-	treatment = j.func(assignment, level, history)
+	#should recieve this from omer
+	history = {"Gender" : "None", "level_of_education" : "None", "enrollment_mode" : "audit",\
+	"ageCategory" : "Null", "ad1" : 1, "ad2" : 1, "ad3" : 1, "ad4" : 1,\
+	"sd1" : 1, "sd2" : 1, "sd3" : 1, "sd4" : 1, \
+	"de1" : 1, "de2" : 1, "de3" : 0.8888888888888888, "de4" : 1,\
+	"score1" : 1, "score2" : 0, "score3" : 0, "score4" : 0}
+
+	lnr = nx.nx_pydot.read_dot('./python/model/{}/{}/pytree.dot'.format(level,asmt))
+	treatment = '1'
+	#TODO deal with possible infinite loop
+	while treatment not in ["A","B","C"]:
+		treatment = getNextNode(history,lnr,treatment)
 	assignment = assignment.replace(" ", "")
 	if treatment == "A":
         #TODO: return correct json based on where the user is
--- python/prediction.jl
+++ python/prediction.jl
@@ -1,3 +1,5 @@
+
+println(time())
 using DataFrames
 using Suppressor
 using Compat: @warn
@@ -6,19 +8,17 @@ using Compat: @warn
 end
 
 function func(assignment, level, history)
-    map=Dict([("Climate Change", "cc"), ("Reading Test Scores", "rts"), ("Detecting Flu Epedemics", "dfe")])
-    asmt = map[assignment]
+    map=Dict([("Climate Change", "cc"), ("Reading Test Scores", "rts"), ("Detecting Flu Epedemics", "dfe")]);
+    asmt = map[assignment];
     #fix this path to be per assignment
-    lnr = OptimalTrees.readjson("./python/model/$level/$asmt/tree.json")
+    lnr = OptimalTrees.readjson("./python/model/$level/$asmt/tree.json");
     #the following function is temporary until i get history from omer
-    history = tempFunc(asmt, level)
-    treatment_prediction, outcome_predictions = OptimalTrees.predict(lnr, history)
-    return treatment_prediction[1]
+    #history = tempFunc(asmt, level)
+    history = DataFrame(Gender = "None", level_of_education = "None", enrollment_mode = "audit", ageCategory = "Null", ad1 = 1, ad2 = 1, ad3 = 1, ad4 = 1, sd1 = 1, sd2 = 1, sd3 = 1, sd4 = 1, de1 = 1, de2 = 1, de3 = 0.8888888888888888, de4 = 1, score1 = 1, score2 = 0, score3 = 0, score4 = 0);
+    treatment_prediction, outcome_predictions = OptimalTrees.predict(lnr, history);
+    return treatment_prediction[1];
 end
 
-#teporary function until history is given
-function tempFunc(assignment, level)
-    df = readtable("./python/model/data/$assignment$level.csv", header=true, makefactors=true)
-    X = df[4:end-3]
-    return X[1,:]
-end
+println(time())
+func("Climate Change", 1, nothing)
+println(time())
