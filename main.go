ed43944a57942de3176c9d8a6f59425f2c6648ab
Omer Arie Goldberg <omergoldberg@fb.com>
2019-02-09 11:06:50 +0200

    server logs are back

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/satori/go.uuid"
 	"html/template"
 	"net/http"
+	"net/http/httputil"
 	"time"
 )
 
@@ -82,11 +83,10 @@ func index(w http.ResponseWriter, req *http.Request) {
 }
 
 func quiz(w http.ResponseWriter, req *http.Request) {
-	// fmt.Println("Incoming request from edx...")
-	// d, _ := httputil.DumpRequest(req, true)
-	// fmt.Println(string(d))
-	// fmt.Println("Request -> ", req)
-	// logPostBody(req)
+	d, _ := httputil.DumpRequest(req, true)
+	fmt.Println(string(d))
+	fmt.Println("Request -> ", req)
+	logPostBody(req)
 	fmt.Println("Incoming request...")
 	if req.Method == http.MethodPost {
 		fmt.Println("Incoming POST req...")
