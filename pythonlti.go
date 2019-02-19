9965935b336f7ecc7d36eda13c7409a8214ca599
Galit <glukin@mit.edu>
2019-02-19 13:48:19 -0500

    fixing go errors

--- pythonlti.go
+++ pythonlti.go
@@ -2,28 +2,19 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
 	"os/exec"
-	"strconv"
-	"strings"
 )
 
 const (
-	Python             = "python3"
-	PathToPythonScript = "./python/transferGrade.py"
+	PathToPythonRequest = "./python/transferGrade.py"
 )
 
 func sendRequest() {
-	cmd := exec.Command(Python, PathToPythonScript)
+	cmd := exec.Command(Python, PathToPythonRequest)
 	outb, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(string(outb))
-	err = json.Unmarshal(outb, &q)
-	if err != nil {
-		fmt.Println(err)
-	}
 	return
 }
