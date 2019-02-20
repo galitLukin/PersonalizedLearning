9dcd534a6b5784e755b16caf1e1578d244d894fd
Galit <glukin@mit.edu>
2019-02-20 17:18:06 -0500

    commenting our request

--- main.go
+++ main.go
@@ -130,54 +130,54 @@ func getStarted(w http.ResponseWriter, req *http.Request) {
 	uid = req.FormValue("user_id")
 	an = req.FormValue("custom_component_display_name")
 	purl = req.FormValue("lis_outcome_service_url")
-	sourcedId := req.FormValue("lis_result_sourcedid")
-	key := req.FormValue("oauth_consumer_key")
-	method := req.FormValue("oauth_signature_method")
-	version := req.FormValue("oauth_version")
-	//signat := req.FormValue("oauth_signature")
-
-	mybody := fmt.Sprintf("<?xml version = \"1.0\" encoding = \"UTF-8\"?><imsx_POXEnvelopeRequest xmlns = \"http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0\"><imsx_POXHeader><imsx_POXRequestHeaderInfo><imsx_version>V1.0</imsx_version><imsx_messageIdentifier>999999123</imsx_messageIdentifier></imsx_POXRequestHeaderInfo></imsx_POXHeader><imsx_POXBody><replaceResultRequest><resultRecord><sourcedGUID><sourcedId>%s</sourcedId></sourcedGUID><result><resultScore><language>en</language><textString>0.92</textString></resultScore></result></resultRecord></replaceResultRequest></imsx_POXBody></imsx_POXEnvelopeRequest>",sourcedId)
-	myr, err := http.NewRequest("POST", purl, bytes.NewBuffer([]byte(mybody)))
-	if err != nil {
-		fmt.Println(err)
-	}
-	var body []byte
-  if myr.Body != nil {
-    var err error
-    body, err = getBody(myr)
-    if err != nil {
-			 fmt.Errorf("Failed to get body of request ...")
-    }
-  }
-	myr.Header.Add("Content-Type", "application/xml; charset=utf-8")
-	hasher := sha1.New()
-	hasher.Write(body)
-	bodyHash := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
-	p := NewProvider("oandgsecret", purl)
-	p.ConsumerKey = "oandgkey"
-	p.Add("oauth_body_hash", bodyHash)
-	p.Add("oauth_consumer_key", key)
-	p.Add("oauth_signature_method", method)
-	p.Add("oauth_version", version)
-	sig, err := Sign(p.values, p.URL, "POST", p.Signer)
-	if err != nil {
-		fmt.Println(err)
-	}
-	oauth_params := fmt.Sprintf("OAuth oauth_body_hash=%s,oauth_consumer_key=%s,oauth_nonce=%s,oauth_signature=%s,oauth_signature_method=%s,oauth_timestamp=%s,oauth_version=%s,", bodyHash, key, nonce(), sig, method, strconv.FormatInt(time.Now().Unix(), 10), version)
-	myr.Header.Add("Authorization", oauth_params)
-	client := &http.Client{}
-	resp, err := client.Do(myr)
-	if err != nil {
-		fmt.Println(err)
-	}
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err2 := ioutil.ReadAll(resp.Body)
-		if err2 != nil {
-			fmt.Println(err)
-		}
-		bodyString := string(bodyBytes)
-		fmt.Println(bodyString)
-	}
+	// sourcedId := req.FormValue("lis_result_sourcedid")
+	// key := req.FormValue("oauth_consumer_key")
+	// method := req.FormValue("oauth_signature_method")
+	// version := req.FormValue("oauth_version")
+	// //signat := req.FormValue("oauth_signature")
+	//
+	// mybody := fmt.Sprintf("<?xml version = \"1.0\" encoding = \"UTF-8\"?><imsx_POXEnvelopeRequest xmlns = \"http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0\"><imsx_POXHeader><imsx_POXRequestHeaderInfo><imsx_version>V1.0</imsx_version><imsx_messageIdentifier>999999123</imsx_messageIdentifier></imsx_POXRequestHeaderInfo></imsx_POXHeader><imsx_POXBody><replaceResultRequest><resultRecord><sourcedGUID><sourcedId>%s</sourcedId></sourcedGUID><result><resultScore><language>en</language><textString>0.92</textString></resultScore></result></resultRecord></replaceResultRequest></imsx_POXBody></imsx_POXEnvelopeRequest>",sourcedId)
+	// myr, err := http.NewRequest("POST", purl, bytes.NewBuffer([]byte(mybody)))
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// var body []byte
+  // if myr.Body != nil {
+  //   var err error
+  //   body, err = getBody(myr)
+  //   if err != nil {
+	// 		 fmt.Errorf("Failed to get body of request ...")
+  //   }
+  // }
+	// myr.Header.Add("Content-Type", "application/xml; charset=utf-8")
+	// hasher := sha1.New()
+	// hasher.Write(body)
+	// bodyHash := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	// p := NewProvider("oandgsecret", purl)
+	// p.ConsumerKey = "oandgkey"
+	// p.Add("oauth_body_hash", bodyHash)
+	// p.Add("oauth_consumer_key", key)
+	// p.Add("oauth_signature_method", method)
+	// p.Add("oauth_version", version)
+	// sig, err := Sign(p.values, p.URL, "POST", p.Signer)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// oauth_params := fmt.Sprintf("OAuth oauth_body_hash=%s,oauth_consumer_key=%s,oauth_nonce=%s,oauth_signature=%s,oauth_signature_method=%s,oauth_timestamp=%s,oauth_version=%s,", bodyHash, key, nonce(), sig, method, strconv.FormatInt(time.Now().Unix(), 10), version)
+	// myr.Header.Add("Authorization", oauth_params)
+	// client := &http.Client{}
+	// resp, err := client.Do(myr)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// if resp.StatusCode == http.StatusOK {
+	// 	bodyBytes, err2 := ioutil.ReadAll(resp.Body)
+	// 	if err2 != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	bodyString := string(bodyBytes)
+	// 	fmt.Println(bodyString)
+	// }
 	// if req.Method != "POST" {
 	// 	http.Error(w, "Only post", 500)
 	// 	return
