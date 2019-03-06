a0198b994793b1e72f185db65d869522af879abe
Galit <glukin@mit.edu>
2019-03-06 18:22:44 -0500

    fixing typo in question

--- python/model/offlineScripts/createCohort.py
+++ python/model/offlineScripts/createCohort.py
@@ -1,6 +1,6 @@
 import pandas as pd
 start = "0226"
-today = "0301"
+today = "0306"
 gradeReport = pd.read_csv("./python/model/data/exp/gradeReport/{}.csv".format(today),header=0)
 gradeReport = gradeReport.loc[gradeReport['Experiment Group (Personalization in Linear Regression Assignment)'] == "Personalized"]
 personalizedUsers = gradeReport.loc[:,'Username']
